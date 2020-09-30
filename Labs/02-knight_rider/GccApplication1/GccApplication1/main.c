2e35b5122d57b3114edb32e48b2704a054e5c719
NechTaSilaSprevadza <jozefmak123456@gmail.com>
2020-09-30 16:00:16 +0200

    LAB 2 Modified

--- Labs/02-knight_rider/GccApplication1/GccApplication1/main.c
+++ Labs/02-knight_rider/GccApplication1/GccApplication1/main.c
@@ -1,4 +1,4 @@
-// Lab_02 - Knight_Rider
+// Adam Budac - Lab_02_Knight_Rider
 
 /* Defines -----------------------------------------------------------*/
 #define LED_RED1    PC1     // AVR pin where red LED is connected
@@ -30,35 +30,35 @@ int main(void)
 	PORTC = PORTC & ~(1<<LED_RED5);
 
 	// Program uses 2 leds shining at once instead of 1 for more KnigtRideder style
-		PORTC = PORTC ^ (1<<LED_RED1);	// zapne 1
+		PORTC = PORTC ^ (1<<LED_RED1);	// turn 1 on
 		_delay_ms(BLINK_DELAY);
 	while (1)
 	{
-		PORTC = PORTC ^ (1<<LED_RED2);	// zapne 2
+		PORTC = PORTC ^ (1<<LED_RED2);	// turn 2 on
 		_delay_ms(BLINK_DELAY);
-		PORTC = PORTC ^ (1<<LED_RED1);	// vypne 1
-		PORTC = PORTC ^ (1<<LED_RED3);	// zapne 3
+		PORTC = PORTC ^ (1<<LED_RED1);	// turn 1 off
+		PORTC = PORTC ^ (1<<LED_RED3);	// turn 3 on
 		_delay_ms(BLINK_DELAY);
-		PORTC = PORTC ^ (1<<LED_RED2);	// vypne 2
-		PORTC = PORTC ^ (1<<LED_RED4);	// zapne 4
+		PORTC = PORTC ^ (1<<LED_RED2);	// turn 2 off
+		PORTC = PORTC ^ (1<<LED_RED4);	// turn 4 on
 		_delay_ms(BLINK_DELAY);
-		PORTC = PORTC ^ (1<<LED_RED3);	// vypne 3
-		PORTC = PORTC ^ (1<<LED_RED5);	// zapne 5
+		PORTC = PORTC ^ (1<<LED_RED3);	// turn 3 off
+		PORTC = PORTC ^ (1<<LED_RED5);	// turn 5 on
 		_delay_ms(BLINK_DELAY);
-		PORTC = PORTC ^ (1<<LED_RED4);	// vypne 4
+		PORTC = PORTC ^ (1<<LED_RED4);	// turn 4 off
 		_delay_ms(BLINK_DELAY);
-		PORTC = PORTC ^ (1<<LED_RED4);	// zapne 4
+		PORTC = PORTC ^ (1<<LED_RED4);	// turn 4 on 
 		_delay_ms(BLINK_DELAY);
-		PORTC = PORTC ^ (1<<LED_RED5);	// vypne 5
-		PORTC = PORTC ^ (1<<LED_RED3);	// zapne 3
+		PORTC = PORTC ^ (1<<LED_RED5);	// turn 5 off
+		PORTC = PORTC ^ (1<<LED_RED3);	// turn 3 on
 		_delay_ms(BLINK_DELAY);
-		PORTC = PORTC ^ (1<<LED_RED4);	// vypne 4
-		PORTC = PORTC ^ (1<<LED_RED2);	// zapne 2
+		PORTC = PORTC ^ (1<<LED_RED4);	// turn 4 off
+		PORTC = PORTC ^ (1<<LED_RED2);	// turn 2 on
 		_delay_ms(BLINK_DELAY);
-		PORTC = PORTC ^ (1<<LED_RED3);	// vypne 3
-		PORTC = PORTC ^ (1<<LED_RED1);	// zapne 1
+		PORTC = PORTC ^ (1<<LED_RED3);	// turn 3 off
+		PORTC = PORTC ^ (1<<LED_RED1);	// turn 1 on
 		_delay_ms(BLINK_DELAY);
-		PORTC = PORTC ^ (1<<LED_RED2);	// vypne 2
+		PORTC = PORTC ^ (1<<LED_RED2);	// turn 2 off
 		_delay_ms(BLINK_DELAY);
 	}
 	return 0;
