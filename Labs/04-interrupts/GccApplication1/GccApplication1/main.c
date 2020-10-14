37702268c708328ca137bfe9812d5daa53fc6927
NechTaSilaSprevadza <jozefmak123456@gmail.com>
2020-10-14 16:09:28 +0200

    Lab 4 Added

---
+++ Labs/04-interrupts/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,71 @@
+
+/* Defines -----------------------------------------------------------*/
+#define LED_D1  PB5
+#define LED_D2  PB4
+#define LED_D3  PB3
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include "gpio.h"           // GPIO library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+
+/* Function definitions ----------------------------------------------*/
+/**
+ * Main function where the program execution begins. Toggle three LEDs
+ * on Multi-function shield with internal 8- and 16-bit timer modules.
+*/
+
+int main(void)
+{
+    /* Configuration of three LEDs */
+    GPIO_config_output(&DDRB, LED_D1);
+    GPIO_write_low(&PORTB, LED_D1);
+    GPIO_config_output(&DDRB, LED_D2);
+    GPIO_write_low(&PORTB, LED_D2);
+    GPIO_config_output(&DDRB, LED_D3);
+    GPIO_write_low(&PORTB, LED_D3);
+
+    /* Configuration of 8-bit Timer/Counter0 */
+    TIM0_overflow_16ms();
+    TIM0_overflow_interrupt_enable();
+
+    /* Configuration of 16-bit Timer/Counter1
+     * Set prescaler and enable overflow interrupt */
+    TIM1_overflow_33ms();
+    TIM1_overflow_interrupt_enable();
+
+    /* Configuration of 8-bit Timer/Counter2 */
+    TIM2_overflow_4ms();
+    TIM2_overflow_interrupt_enable();
+
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Infinite loop
+    while (1)
+    {
+        /* Empty loop. All subsequent operations are performed exclusively 
+         * inside interrupt service routines ISRs */
+    }
+
+    // Will never reach this
+    return 0;
+}
+
+/* Interrupt service routines ----------------------------------------*/
+/**
+ * ISR starts when Timer/Counter1/2/3 overflows. Toggle LED D1/2/3 on 
+ * Multi-function shield. */
+ISR(TIMER0_OVF_vect)
+{
+    GPIO_toggle(&PORTB, LED_D1);
+}
+ISR(TIMER1_OVF_vect)
+{
+	GPIO_toggle(&PORTB, LED_D2);
+}
+ISR(TIMER2_OVF_vect)
+{
+	GPIO_toggle(&PORTB, LED_D3);
+}
