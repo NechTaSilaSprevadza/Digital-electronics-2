a63acf781a3c3469f359678dbce5ced3286646db
NechTaSilaSprevadza <jozefmak123456@gmail.com>
2020-12-09 13:42:13 +0100

    Project updated

--- Labs/project/Libraries/lcd_definitions.h
+++ Labs/project/Libraries/lcd_definitions.h
@@ -35,10 +35,10 @@
 #define LCD_DATA1_PIN   PD5     // HD44780 pin D5
 #define LCD_DATA2_PIN   PD6     // HD44780 pin D6
 #define LCD_DATA3_PIN   PD7     // HD44780 pin D7
-#define LCD_RS_PORT     PORTB
-#define LCD_RS_PIN      PB0
-#define LCD_E_PORT      PORTB
-#define LCD_E_PIN       PB1
+#define LCD_RS_PORT     PORTD
+#define LCD_RS_PIN      PD2
+#define LCD_E_PORT      PORTD
+#define LCD_E_PIN       PD3
 // R/W pin is connected to GND on LCD Keypad Shield
 
 #endif
